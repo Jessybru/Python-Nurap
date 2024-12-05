b7add7cc87bf514b5fffdbe3c40cf2427bf0bec3
Jessybru <jessika123pp@gmail.com>
2024-12-05 10:01:46 -0300

    Add files via upload

---
+++ AULA 2/AULA 02.py
@@ -0,0 +1,11 @@
+print('melhor esta bem em um lugar, do que estar e nao ta!!')
+      
+nome_usuario = 'Jessika Santiago' #nome
+idade_usuario = 17 #idade 
+altura_usuario = 1,54 #altura 
+maior_idade = False 
+
+print('Seja Bem Vindo', nome_usuario )
+print('sua idade e', idade_usuario,'anos')
+print('voce tem' , altura_usuario, 'metros')
+print('voce e maior de idade?', maior_idade)
